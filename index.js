57ce0619a81608f39a4ab589651bc0a2a25cd709
Kevin L <134300540+Squog-The-Scriptor@users.noreply.github.com>
2024-01-17 23:03:46 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,50 @@
+let homeScore = 0
+let guestScore = 0
+
+let homeEl = document.getElementById("home-score")
+let guestEl = document.getElementById("guest-score")
+
+
+/* Home Tracker*/
+function plusOne() {
+    homeScore += 1
+    homeEl.textContent = homeScore
+    
+}
+
+function plusTwo() {
+    homeScore += 2
+    homeEl.textContent = homeScore
+    
+}
+
+
+function plusThree() {
+    homeScore += 3
+    homeEl.textContent = homeScore
+}
+
+/* Guest Tracker*/
+function plusOneG() {
+    guestScore += 1
+    guestEl.textContent = guestScore
+    
+}
+
+function plusTwoG() {
+    guestScore += 2
+    guestEl.textContent = guestScore
+    
+}
+
+function plusThreeG() {
+    guestScore += 3
+    guestEl.textContent = guestScore
+}
+
+function reset() {
+    guestScore = 0
+    homeScore = 0
+    guestEl.textContent = 0
+    homeEl.textContent = 0
+}
