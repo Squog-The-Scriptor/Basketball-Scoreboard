e11212da37756e7cc63e497032ce558f2a73eb52
Kevin L <134300540+Squog-The-Scriptor@users.noreply.github.com>
2024-01-17 21:32:44 -0500

    First commit

---
+++ index.js
@@ -0,0 +1,46 @@
+let homeScore = 0
+let guestScore = 0
+
+let homeEl = document.getElementById("home-score")
+let guestEl = document.getElementById("guest-score")
+
+function plusOne() {
+    homeScore += 1
+    homeEl.textContent = homeScore
+    
+}
+
+function plusTwo() {
+    homeScore += 2
+    homeEl.textContent = homeScore
+    
+}
+
+
+function plusThree() {
+    homeScore += 3
+    guestEl.textContent = homeScore
+}
+
+/* Guest Tracker*/
+function plusOneG() {
+    guestScore += 1
+    guestEl.textContent = guestScore
+    
+}
+
+function plusTwoG() {
+    guestScore += 2
+    guestEl.textContent = guestScore
+    
+}
+
+function plusThreeG() {
+    guestScore += 3
+    guestEl.textContent = guestScore
+}
+
+function reset() {
+    guestEl.textContent = 0
+    homeEl.textContent = 0
+}
